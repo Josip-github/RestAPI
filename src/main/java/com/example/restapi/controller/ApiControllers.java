38848ea99cac15515bca4df58566afa994a20ea0
Josip-github <josipcota1@gmail.com>
2021-09-13 10:31:33 +0200

    Method updateUser This time we use @PutMapping and this method assumes that there is at least 1 row in a table, if not, then this method cannot work.

--- src/main/java/com/example/restapi/controller/ApiControllers.java
+++ src/main/java/com/example/restapi/controller/ApiControllers.java
@@ -3,10 +3,7 @@ package com.example.restapi.controller;
 import com.example.restapi.model.User;
 import com.example.restapi.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,4 +29,14 @@ public class ApiControllers {
         return "Saved...";
     }
 
+    @PutMapping(value = "/update/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user){
+        User updatedUser = userRepo.findById(id).get();
+        updatedUser.setFirstname(user.getFirstname());
+        updatedUser.setLastname(user.getLastname());
+        updatedUser.setOccupation(user.getOccupation());
+        updatedUser.setAge(user.getAge());
+        userRepo.save(updatedUser);
+        return "Updated...";
+    }
 }
