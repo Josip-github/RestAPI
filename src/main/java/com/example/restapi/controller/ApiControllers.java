f27d0227f2bb66776a2a371b8c62d1d8704887d5
Josip-github <josipcota1@gmail.com>
2021-09-13 10:22:42 +0200

    Method getUsers First of all we have to instantiate userRepo interface in the class ApiControllers with @Autowired Method getUsers has access to the private UserRepo userRepo since they are in the same class. userRepo has access to the method findAll because interface UserRepo extends JpaRepository.
    Annotation for this method has to be: @GetMapping

--- src/main/java/com/example/restapi/controller/ApiControllers.java
+++ src/main/java/com/example/restapi/controller/ApiControllers.java
@@ -1,13 +1,27 @@
 package com.example.restapi.controller;
 
+import com.example.restapi.model.User;
+import com.example.restapi.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ApiControllers {
+
+    @Autowired
+    private UserRepo userRepo;
 
     @GetMapping
     public String getPage(){
         return "Welcome!";
     }
+
+    @GetMapping(value = "/users")
+    public List<User> getUsers(){
+        return userRepo.findAll();
+    }
+
 }
