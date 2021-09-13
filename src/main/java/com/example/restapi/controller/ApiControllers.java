9b36e54ecef8c53268cae7165f023f4a08c96c4a
Josip-github <josipcota1@gmail.com>
2021-09-13 10:26:04 +0200

    Method saveUser The annotation is @PostMapping because this time we don't want just to get some data, but create them. @RequestBody - we need this because of Postman

--- src/main/java/com/example/restapi/controller/ApiControllers.java
+++ src/main/java/com/example/restapi/controller/ApiControllers.java
@@ -4,6 +4,8 @@ import com.example.restapi.model.User;
 import com.example.restapi.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,6 +24,12 @@ public class ApiControllers {
     @GetMapping(value = "/users")
     public List<User> getUsers(){
         return userRepo.findAll();
+    }
+
+    @PostMapping(value = "/save")
+    public String saveUser(@RequestBody User user){
+        userRepo.save(user);
+        return "Saved...";
     }
 
 }
